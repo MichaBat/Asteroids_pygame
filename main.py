093231df8ee21eb59123a5131e468b2a28d2a5e4
micha <michabatelaan@gmail.com>
2026-01-02 14:50:47 +0100

    CH3:L5

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 import pygame
 
 
@@ -14,6 +15,14 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable  = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
+
     while True:
         log_state()
 
@@ -22,7 +31,13 @@ def main():
                 return
 
         screen.fill("black")
+        updatable.update(dt)
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
